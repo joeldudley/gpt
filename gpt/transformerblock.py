0fe728c87900aa10c2df2c9ba5d7f52362d5c055
joeldudley <joel.l.dudley@gmail.com>
2024-04-10 13:33:53 +0100

    Pull forward pass logic into TransformerBlockFeedForward

--- gpt/transformerblock.py
+++ gpt/transformerblock.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 
-from gpt.constants import EMBED_DIM, DROPOUT_PROB
-from gpt.gelu import GaussianErrorLinearUnit
+from gpt.constants import EMBED_DIM
 from gpt.multiheadattention import MultiHeadAttention
+from gpt.transformerblockfeedforward import TransformerBlockFeedForward
 
 
 class TransformerBlock(nn.Module):
@@ -11,22 +11,11 @@ class TransformerBlock(nn.Module):
         self.layer_norm_1 = nn.LayerNorm(EMBED_DIM)
         self.attention_block = MultiHeadAttention(max_seq_len)
         self.layer_norm_2 = nn.LayerNorm(EMBED_DIM)
-        self.feedforward = nn.ModuleDict(dict(
-            output_linear_transform=nn.Linear(EMBED_DIM, 4 * EMBED_DIM),
-            # todo - joel - rename to output_projection
-            c_proj=nn.Linear(4 * EMBED_DIM, EMBED_DIM),
-            activation=GaussianErrorLinearUnit(),
-            dropout=nn.Dropout(DROPOUT_PROB),
-        ))
+        self.feedforward = TransformerBlockFeedForward()
 
     def forward(self, inputs):
         attn_outputs = self.attention_block(self.layer_norm_1(inputs))
         attn_outputs_with_skip = attn_outputs + inputs
         attn_outputs_normed = self.layer_norm_2(attn_outputs_with_skip)
-        feedforward_outputs = self._feedforward(attn_outputs_normed)
+        feedforward_outputs = self.feedforward(attn_outputs_normed)
         return feedforward_outputs + attn_outputs_with_skip
-
-    def _feedforward(self, x):
-        activations = self.feedforward.activation(self.feedforward.output_linear_transform(x))
-        projections = self.feedforward.c_proj(activations)
-        return self.feedforward.dropout(projections)
---
+++ gpt/transformerblockfeedforward.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+from gpt.constants import EMBED_DIM, DROPOUT_PROB
+from gpt.gelu import GaussianErrorLinearUnit
+
+
+class TransformerBlockFeedForward(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.output_linear_transform = nn.Linear(EMBED_DIM, 4 * EMBED_DIM)
+        # todo - joel - rename to output_projection
+        self.c_proj = nn.Linear(4 * EMBED_DIM, EMBED_DIM)
+        self.activation = GaussianErrorLinearUnit()
+        self.dropout = nn.Dropout(DROPOUT_PROB)
+
+    def forward(self, inputs):
+        linear_transform = self.output_linear_transform(inputs)
+        activations = self.activation(linear_transform)
+        projections = self.c_proj(activations)
+        return self.dropout(projections)
