8d5b3a11d3981e12f04b6ce5bb15ebc502d4aa72
joeldudley <joel.l.dudley@gmail.com>
2024-04-11 10:14:57 +0100

    Refactoring

--- tests/addition/datasets.py
+++ tests/addition/datasets.py
@@ -20,28 +20,24 @@ def get_train_test_datasets():
     dataset_size = (10 ** NUM_DIGITS) ** 2
     test_set_size = min(int(dataset_size * 0.2), MAX_TEST_SET_SIZE)  # 20% of the whole dataset, max. 500.
 
-    samples = _get_all_samples(NUM_DIGITS, dataset_size)
+    all_number_pairs = torch.randperm(dataset_size, generator=torch.Generator().manual_seed(RANDOM_SEED))
+    samples = [_get_sample(tensor.item()) for tensor in all_number_pairs]
     return SimpleDataset(samples[test_set_size:]), SimpleDataset(samples[:test_set_size])
 
 
-def _get_all_samples(num_digits, dataset_size):
-    all_number_pairs = torch.randperm(dataset_size, generator=torch.Generator().manual_seed(RANDOM_SEED))
-    return [_get_sample(tensor.item(), num_digits) for tensor in all_number_pairs]
-
-
-def _get_sample(number_pair, num_digits):
-    num_width = 10 ** num_digits
+def _get_sample(number_pair):
+    num_width = 10 ** NUM_DIGITS
     a = number_pair // num_width
     b = number_pair % num_width
     target = a + b
 
-    a_str = str(a).zfill(num_digits)
-    b_str = str(b).zfill(num_digits)
-    target_str = str(target).zfill(num_digits + 1)[::-1]  # It's easier to learn addition if the sum is flipped.
+    a_str = str(a).zfill(NUM_DIGITS)
+    b_str = str(b).zfill(NUM_DIGITS)
+    target_str = str(target).zfill(NUM_DIGITS + 1)[::-1]  # It's easier to learn addition if the sum is flipped.
 
     samples = [int(s) for s in a_str + b_str + target_str]
     inputs = torch.tensor(samples[:-1], dtype=torch.long)
     targets = torch.tensor(samples[1:], dtype=torch.long)
-    targets[:num_digits * 2 - 1] = -1
+    targets[:NUM_DIGITS * 2 - 1] = -1
 
-    return inputs, targets  # e.g. `96 + 29 = 125` becomes inputs = [9, 6, 2, 9, 5, 2]; targets = [-1, -1, -1, 5, 2, 1]
+    return inputs, targets  # e.g. `96 + 29 = 125` becomes inputs = [9, 6, 2, 9, 5, 2], targets = [-1, -1, -1, 5, 2, 1]
