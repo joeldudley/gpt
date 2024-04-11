1ca797f0aab4b1d710db77a54551b69920cee708
joeldudley <joel.l.dudley@gmail.com>
2024-04-11 10:16:57 +0100

    Refactoring

--- tests/addition/datasets.py
+++ tests/addition/datasets.py
@@ -35,9 +35,9 @@ def _get_sample(number_pair):
     b_str = str(b).zfill(NUM_DIGITS)
     target_str = str(target).zfill(NUM_DIGITS + 1)[::-1]  # It's easier to learn addition if the sum is flipped.
 
+    # e.g. `96 + 29 = 125` becomes inputs = [9, 6, 2, 9, 5, 2], targets = [-1, -1, -1, 5, 2, 1]
     samples = [int(s) for s in a_str + b_str + target_str]
     inputs = torch.tensor(samples[:-1], dtype=torch.long)
     targets = torch.tensor(samples[1:], dtype=torch.long)
     targets[:NUM_DIGITS * 2 - 1] = -1
-
-    return inputs, targets  # e.g. `96 + 29 = 125` becomes inputs = [9, 6, 2, 9, 5, 2], targets = [-1, -1, -1, 5, 2, 1]
+    return inputs, targets
