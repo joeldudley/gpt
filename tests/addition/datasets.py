c1bd5f3af7c263f6606d59ccc07171a00355ab18
joeldudley <joel.l.dudley@gmail.com>
2024-04-12 13:20:22 +0100

    Refactoring

--- tests/addition/datasets.py
+++ tests/addition/datasets.py
@@ -25,19 +25,20 @@ def get_train_test_datasets():
     return SimpleDataset(samples[test_set_size:]), SimpleDataset(samples[:test_set_size])
 
 
-def _get_sample(number_pair):
-    num_width = 10 ** NUM_DIGITS
-    a = number_pair // num_width
-    b = number_pair % num_width
-    target = a + b
+# e.g. `9629` becomes 96 + 29 = 125 becomes inputs = `[9, 6, 2, 9, 5, 2]`, targets = `[-1, -1, -1, 5, 2, 1]`
+def _get_sample(number_pair: int):
+    second_num_max = 10 ** NUM_DIGITS
+    first_num = number_pair // second_num_max
+    second_num = number_pair % second_num_max
+    target = first_num + second_num
 
-    a_str = str(a).zfill(NUM_DIGITS)
-    b_str = str(b).zfill(NUM_DIGITS)
+    first_num_str = str(first_num).zfill(NUM_DIGITS)
+    second_num_str = str(second_num).zfill(NUM_DIGITS)
     target_str = str(target).zfill(NUM_DIGITS + 1)[::-1]  # It's easier to learn addition if the sum is flipped.
 
     # e.g. `96 + 29 = 125` becomes inputs = [9, 6, 2, 9, 5, 2], targets = [-1, -1, -1, 5, 2, 1]
-    samples = [int(s) for s in a_str + b_str + target_str]
-    inputs = torch.tensor(samples[:-1], dtype=torch.long)
-    targets = torch.tensor(samples[1:], dtype=torch.long)
+    samples = [int(s) for s in first_num_str + second_num_str + target_str]
+    inputs = torch.tensor(samples[:-1])
+    targets = torch.tensor(samples[1:])
     targets[:NUM_DIGITS * 2 - 1] = -1
     return inputs, targets
