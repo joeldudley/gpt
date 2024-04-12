db544b08cb4e5f2dc818426b20bdc7614950071e
joeldudley <joel.l.dudley@gmail.com>
2024-04-12 13:35:43 +0100

    Refactoring

--- gpt/transformer/transformerblock.py
+++ gpt/transformer/transformerblock.py
@@ -8,16 +8,16 @@ from gpt.transformer.transformerblockfeedforward import TransformerBlockFeedForw
 class TransformerBlock(nn.Module):
     def __init__(self, max_seq_len):
         super().__init__()
-        self.norm_1 = nn.LayerNorm(EMBED_DIM)
+        self.norm_inputs = nn.LayerNorm(EMBED_DIM)
         self.multi_head_attention = MultiHeadAttention(max_seq_len)
-        self.norm_2 = nn.LayerNorm(EMBED_DIM)
+        self.norm_multi_head_attention = nn.LayerNorm(EMBED_DIM)
         self.feedforward = TransformerBlockFeedForward()
 
     def forward(self, inputs):
-        inputs_normed = self.norm_1(inputs)
+        inputs_normed = self.norm_inputs(inputs)
         attn_outputs = self.multi_head_attention(inputs_normed)
         attn_outputs_with_skip = attn_outputs + inputs
-        attn_outputs_normed = self.norm_2(attn_outputs_with_skip)
+        attn_outputs_normed = self.norm_multi_head_attention(attn_outputs_with_skip)
         feedforward_outputs = self.feedforward(attn_outputs_normed)
         feedforward_outputs_with_skip = feedforward_outputs + attn_outputs_with_skip
         return feedforward_outputs_with_skip
