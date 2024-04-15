86b47603bdb1e145ee3240c6f4c6fdef6fea326e
joeldudley <joel.l.dudley@gmail.com>
2024-04-15 13:54:49 +0100

    Refactoring

--- gpt/train/optimisation.py
+++ gpt/train/optimisation.py
@@ -5,22 +5,14 @@ from gpt.constants import WEIGHT_DECAY
 
 
 def get_adamw_optimizer(named_modules, named_params):
-    decay_params, no_decay_params = _get_params_by_type(named_modules, named_params)
+    decay_params, no_decay_params = _get_params_groups(named_modules, named_params)
     optimiser_groups = [{"params": decay_params, "weight_decay": WEIGHT_DECAY},
                         {"params": no_decay_params, "weight_decay": 0.0}]
     return AdamW(optimiser_groups)
 
 
-def _get_params_by_type(named_modules, named_params):
-    params_to_decay = _get_linear_weight_params(named_modules)
-    param_dict = {param_name: param for param_name, param in named_params}
-    decay_params = [param_dict[param_name] for param_name in params_to_decay]
-    no_decay_params = [param_dict[param_name] for param_name in param_dict.keys() - params_to_decay]
-    return decay_params, no_decay_params
-
-
-def _get_linear_weight_params(named_modules):
-    return {(module_name + '.' if module_name else '') + param_name
-            for module_name, module in named_modules
-            for param_name, _ in module.named_parameters()
-            if param_name.endswith('weight') and isinstance(module, torch.nn.Linear)}
+def _get_params_groups(named_modules, named_params):
+    all_params = {param for _, param in named_params}
+    params_to_decay = {param for _, module in named_modules for param_name, param in module.named_parameters()
+                       if param_name.endswith('weight') and isinstance(module, torch.nn.Linear)}
+    return list(params_to_decay), list(all_params - params_to_decay)
